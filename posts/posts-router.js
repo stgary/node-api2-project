81fb8a4c5218bf25fc6a6d255f044bf6d6f1a7e4
Stephen Gary <sgary0@protonmail.com>
2020-09-01 14:31:43 -0400

    router set up

---
+++ posts/posts-router.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const db = require('../data/db.js');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/', (req, res) => {
+
+});
+
+router.put('/', (req, res) => {
+
+});
+
+router.delete('/', (req, res) => {
+
+});
+
+module.exports = router;
